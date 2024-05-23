ec5fa9e7b62d0f2e6cc2a290189ecdf3bb3cf570
hoangcaominh <hoangcaominh1812@gmail.com>
2024-05-23 23:33:16 +0700

    Big overhaul
    - Split update status function into smaller functions
    - Use miss/bomb state instead of percentage difference
    - Use background worker instead of Invoke

--- IC4128LS/MainWindow.cs
+++ IC4128LS/MainWindow.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Drawing;
-using System.Threading.Tasks;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -12,14 +10,15 @@ namespace IC4128LS
         private readonly THProcess thprocess = new THProcess();
 
         // medal_state is needed for medal counting
-        // short: stage, miss, lives, bomb, gold, silver, bronze;
-        // int: score
         private byte difficulty, medal_state, _medal_state;
         private ushort stage, miss, bomb, gold, silver, bronze;
-        private int lives, _lives, pf, _pf;
+        private uint _miss_state, miss_state, _bomb_state, bomb_state;
         private uint p_medal_state, frame_count, score;
 
-        private float iscore;
+        private readonly string[] ArrDiff = { "Easy", "Normal", "Hard", "Lunatic", "Extra" };
+        private readonly string[] ArrRoute = { "?", "A", "A1", "A1", "A2", "A2", "B", "B1", "B1", "B2", "B2", "C", "C1", "C1", "C2", "C2", "Ex" };
+
+        // private float iscore;
 
         private string lang;
 
@@ -63,7 +62,7 @@ namespace IC4128LS
             FindButton.Enabled = false;
             LabelFound.Visible = true;
             LabelNotFound.Visible = false;
-            new Task(UpdateStatus).Start();
+            mainWorker.RunWorkerAsync();
         }
 
         private void Find_NotFound()
@@ -87,75 +86,94 @@ namespace IC4128LS
             }
         }
 
-        private readonly string[] ArrDiff = { "Easy", "Normal", "Hard", "Lunatic", "Extra" };
-        private readonly string[] ArrRoute = { "?", "A", "A1", "A1", "A2", "A2", "B", "B1", "B1", "B2", "B2", "C", "C1", "C1", "C2", "C2", "Ex" };
-        
-        private void UpdateStatus()
+        private void OnClosing(object sender, FormClosingEventArgs e)
         {
-            do
-            {
-                // Read memory
-                ReadMemory(0x004B4D20, 0, sizeof(uint), ref frame_count);
-                ReadMemory(0x004B4D0C, 0, sizeof(byte), ref difficulty);
-                ReadMemory(0x004B4D14, 0, sizeof(ushort), ref stage);
-                ReadMemory(0x004B4D64, 0, sizeof(uint), ref lives);
-                ReadMemory(0x004B4D70, 0, sizeof(uint), ref pf);
-                ReadMemory(0x004B4CC4, 0, sizeof(uint), ref score);
-                ReadMemory(0x004B8934, 0, sizeof(uint), ref p_medal_state);
-                ReadMemory(p_medal_state, 0x7C, sizeof(byte), ref medal_state);
+            if (mainWorker.IsBusy)
+                mainWorker.CancelAsync();
+        }
 
-                // Calculate
-                lock (this)
+        private void ReadMemory()
+        {
+            uint p_miss_state = 0, p_bomb_state = 0;
+            thprocess.ReadMemory(0x004B4D20, 0, sizeof(uint), ref frame_count);
+            thprocess.ReadMemory(0x004B4D0C, 0, sizeof(byte), ref difficulty);
+            thprocess.ReadMemory(0x004B4D14, 0, sizeof(ushort), ref stage);
+            thprocess.ReadMemory(0x004B8A80, 0, sizeof(uint), ref p_miss_state);
+            thprocess.ReadMemory(p_miss_state, 0xF78, sizeof(byte), ref miss_state);
+            thprocess.ReadMemory(0x004B892C, 0, sizeof(uint), ref p_bomb_state);
+            thprocess.ReadMemory(p_bomb_state, 0x3C, sizeof(byte), ref bomb_state);
+            thprocess.ReadMemory(0x004B4CC4, 0, sizeof(uint), ref score);
+            thprocess.ReadMemory(0x004B8934, 0, sizeof(uint), ref p_medal_state);
+            thprocess.ReadMemory(p_medal_state, 0x7C, sizeof(byte), ref medal_state);
+        }
+
+        private void UpdateLogic()
+        {
+            // reset
+            if (frame_count <= 0x0F && score == 0)
+                miss = bomb = gold = silver = bronze = 0;
+            // miss count - 0: reviving; 1: alive; 2: dying; 4: border of life and death
+            if (miss_state != _miss_state && miss_state == 2)
+                miss++;
+            _miss_state = miss_state;
+            // bomb count - 0: not bombing; 1: bombing
+            if (bomb_state != _bomb_state && bomb_state == 1)
+                bomb++;
+            _bomb_state = bomb_state;
+            // medal count
+            if ((_medal_state & 128) > 0 && (medal_state & 128) == 0)
+            {
+                if ((medal_state & 4) > 0)
+                    gold++;
+                else if ((medal_state & 2) > 0)
+                    silver++;
+                else
+                    bronze++;
+            }
+            _medal_state = medal_state;
+            // iscore
+            // if (difficulty == 3)
+            // iscore = gold * 1.5f - miss - bomb * 2;
+        }
+
+        private void UpdateUI()
+        {
+            ValueDiff.Text = ArrDiff[difficulty];
+            ValueRoute.Text = ArrRoute[stage];
+            ValueMiss.Text = miss.ToString();
+            ValueBomb.Text = bomb.ToString();
+            ValueGold.Text = gold.ToString();
+            ValueSilver.Text = silver.ToString();
+            ValueBronze.Text = bronze.ToString();
+            // ValueISCORE.Text = iscore.ToString();
+        }
+
+        private void Worker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (thprocess.FindProcess())
+            {
+                if (mainWorker.CancellationPending)
                 {
-                    // reset
-                    if (frame_count <= 0x0F && score == 0)
-                        miss = bomb = gold = silver = bronze = 0;
-                    // miss count
-                    // 10000 / 2 because fast forward can make the resource reduce less than 10000. Why not 1? Because Marisa laser exists
-                    if (lives < _lives - 10000 / 2)
-                        miss++;
-                    _lives = lives;
-                    // bomb count
-                    if (pf < _pf - 10000 / 2)
-                        bomb++;
-                    _pf = pf;
-                    // medal count
-                    if ((_medal_state & 128) > 0 && (medal_state & 128) == 0)
-                    {
-                        if ((medal_state & 4) > 0)
-                            gold++;
-                        else if ((medal_state & 2) > 0)
-                            silver++;
-                        else
-                            bronze++;
-                    }
-                    _medal_state = medal_state;
-                    // iscore
-                    // if (difficulty == 3)
-                    iscore = gold * 1.5f - miss - bomb * 2;
+                    e.Cancel = true;
+                    return;
                 }
 
-                // Update UI
-                Invoke(new Action(() =>
-                {
-                    ValueDiff.Text = ArrDiff[difficulty];
-                    ValueRoute.Text = ArrRoute[stage];
-                    ValueMiss.Text = miss.ToString();
-                    ValueBomb.Text = bomb.ToString();
-                    ValueGold.Text = gold.ToString();
-                    ValueSilver.Text = silver.ToString();
-                    ValueBronze.Text = bronze.ToString();
-                    ValueISCORE.Text = iscore.ToString();
-                }));
-                Thread.Sleep(10);
-            }
-            while (FindProcess());
+                ReadMemory();
+                UpdateLogic();
 
-            // Display info after closing the game
-            Invoke(new Action(() =>
-            {
-                Find_NotFound();
-            }));
+                mainWorker.ReportProgress(9);
+                Thread.Sleep(15);
+            }
+        }
+
+        private void Worker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            Find_NotFound();
         }
     }
 }
