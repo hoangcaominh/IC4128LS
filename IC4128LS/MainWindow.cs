fc3f4df04b4457c5d45fe372709ffc09c7fbec43
hoangcaominh <hoangcaominh1812@gmail.com>
2024-05-23 23:31:36 +0700

    Move process reading to THProcess class

--- IC4128LS/MainWindow.cs
+++ IC4128LS/MainWindow.cs
@@ -1,71 +1,15 @@
 ﻿using System;
 using System.IO;
 using System.Drawing;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Threading;
-using System.Diagnostics;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
 
 namespace IC4128LS
 {
     public partial class MainWindow : Form
     {
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
-
-        private IntPtr handle;
-
-        private bool FindProcess()
-        {
-            var proc = Process.GetProcessesByName("th128");
-            if (proc.Length > 0)
-            {
-                // PROCESS_VM_READ (0x10)
-                handle = OpenProcess(0x10, false, proc.FirstOrDefault().Id);
-                return handle != null;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private void ReadMemory(uint address, int offset, int size, ref byte target)
-        {
-            byte[] buffer = new byte[1];
-            IntPtr bytesRead = IntPtr.Zero;
-            bool read = ReadProcessMemory(handle, (IntPtr)(address + offset), buffer, size, out bytesRead);
-            target = buffer[0];
-        }
-
-        private void ReadMemory(uint address, int offset, int size, ref ushort target)
-        {
-            byte[] buffer = new byte[2];
-            IntPtr bytesRead = IntPtr.Zero;
-            bool read = ReadProcessMemory(handle, (IntPtr)(address + offset), buffer, size, out bytesRead);
-            target = BitConverter.ToUInt16(buffer, 0);
-        }
-
-        private void ReadMemory(uint address, int offset, int size, ref int target)
-        {
-            byte[] buffer = new byte[4];
-            IntPtr bytesRead = IntPtr.Zero;
-            bool read = ReadProcessMemory(handle, (IntPtr)(address + offset), buffer, size, out bytesRead);
-            target = BitConverter.ToInt32(buffer, 0);
-        }
-
-        private void ReadMemory(uint address, int offset, int size, ref uint target)
-        {
-            byte[] buffer = new byte[4];
-            IntPtr bytesRead = IntPtr.Zero;
-            bool read = ReadProcessMemory(handle, (IntPtr)(address + offset), buffer, size, out bytesRead);
-            target = BitConverter.ToUInt32(buffer, 0);
-        }
+        private readonly THProcess thprocess = new THProcess();
 
         // medal_state is needed for medal counting
         // short: stage, miss, lives, bomb, gold, silver, bronze;
@@ -133,7 +77,7 @@ namespace IC4128LS
         {
             // Lang lang = new Lang();
             // lang.Show();
-            if (FindProcess())
+            if (thprocess.FindProcess())
             {
                 Find_Found();
             }
---
+++ IC4128LS/THProcess.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace IC4128LS
+{
+    internal class THProcess
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+        private IntPtr handle;
+
+        public IntPtr Handle { get => handle; }
+
+        public bool FindProcess()
+        {
+            var proc = Process.GetProcessesByName("th128");
+            if (proc.Length > 0)
+            {
+                // PROCESS_VM_READ (0x10)
+                handle = OpenProcess(0x10, false, proc.FirstOrDefault().Id);
+                return handle != null;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void ReadMemory(uint address, int offset, int size, ref byte target)
+        {
+            byte[] buffer = new byte[1];
+            IntPtr bytesRead = IntPtr.Zero;
+            bool read = ReadProcessMemory(handle, (IntPtr)(address + offset), buffer, size, out bytesRead);
+            target = buffer[0];
+        }
+
+        public void ReadMemory(uint address, int offset, int size, ref ushort target)
+        {
+            byte[] buffer = new byte[2];
+            IntPtr bytesRead = IntPtr.Zero;
+            bool read = ReadProcessMemory(handle, (IntPtr)(address + offset), buffer, size, out bytesRead);
+            target = BitConverter.ToUInt16(buffer, 0);
+        }
+
+        public void ReadMemory(uint address, int offset, int size, ref int target)
+        {
+            byte[] buffer = new byte[4];
+            IntPtr bytesRead = IntPtr.Zero;
+            bool read = ReadProcessMemory(handle, (IntPtr)(address + offset), buffer, size, out bytesRead);
+            target = BitConverter.ToInt32(buffer, 0);
+        }
+
+        public void ReadMemory(uint address, int offset, int size, ref uint target)
+        {
+            byte[] buffer = new byte[4];
+            IntPtr bytesRead = IntPtr.Zero;
+            bool read = ReadProcessMemory(handle, (IntPtr)(address + offset), buffer, size, out bytesRead);
+            target = BitConverter.ToUInt32(buffer, 0);
+        }
+    }
+}
